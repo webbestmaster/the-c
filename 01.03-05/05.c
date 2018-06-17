58cdc000b9cbfc240f3702bb14fddc4999aeddf3
dim <web.best.master@gmail.com>
2018-06-17 20:54:15 +0300

    01.05

--- 01.03-05/05.c
+++ 01.03-05/05.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define LOWER 0 // нижняя граница таблицы
+#define UPPER 300 // верхняя граница таблицы
+#define STEP 20 // размер шага
+
 int main() {
     int farh;
 
@@ -12,6 +16,12 @@ int main() {
     // backward
     printf("--- backward ---\n");
     for (farh = 300; farh >= 0; farh -= 20) {
+        printf("%3d %6.1f\n", farh, (5.0 / 9.0) * (farh - 32));
+    }
+
+    // use 'defined variables'
+    printf("--- defined variables ---\n");
+    for (farh = LOWER; farh <= UPPER; farh += STEP) {
         printf("%3d %6.1f\n", farh, (5.0 / 9.0) * (farh - 32));
     }
 
